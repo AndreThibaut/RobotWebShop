cbdb1ff38ec9161536453aea36ccc3de6e3f3bbd
AndreThibaut <and.thibaut@gmail.com>
2019-10-06 16:50:27 +0200

    minor change

--- RobotWebShop/Controllers/UserController.cs
+++ RobotWebShop/Controllers/UserController.cs
@@ -9,7 +9,6 @@ using RobotWebShop.Application.StockAdmin;
 using RobotWebShop.Application.UsersAdmin;
 using RobotWebShop.Database;
 
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace RobotWebShop.Controllers
 {
